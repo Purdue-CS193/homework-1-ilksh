5775c1487d81649a8c13533658a9ad174dae522e
Sang Hyun Kim <104607302+ilksh@users.noreply.github.com>
2022-09-15 17:07:15 -0400

    Update answers.sh

--- answers.sh
+++ answers.sh
@@ -4,37 +4,37 @@
 cd ORIGIN
 
 # Task 1 
-
+mv ~/.Homework/MA165/HW2.txt ~/HW1.txt
 
 # Task 2
-
+rm ~/Memes/SpongeBob/money.jpeg
 
 # Task 3
-
+mv ~/fafs/jpg ~/Memes/SpongeBob
 
 # Task 4
-
+rm -r ~/Homework/CS191
 
 # Task 5
-
+mkdir Programs/C++
 
 # Task 6
-
+mv ~/Programs/Age.cpp ~/C++
 
 # Task 7
-
+cp ~/.Programs/Age.cpp ~/C++
 
 # Task 8
-
+mv Programs/Count.c Programs/Goodbye.c Programs/C
 
 # Task 9
-
+mv Programs/Name.py Programs/Space.py Programs/'Python 3'
 
 # Task 10
-
+touch Homework/POL141/Essay.txt
 
 # Task 11
-
+ls
 
 # Task 12
 : '
