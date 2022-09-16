e48d931ba3194c18bd0f92e7242d77bbdc00c1ef
Sang Hyun Kim <104607302+ilksh@users.noreply.github.com>
2022-09-16 01:15:19 -0400

    Update answers.sh

--- answers.sh
+++ answers.sh
@@ -4,13 +4,13 @@
 cd ORIGIN
 
 # Task 1 
-mv Homework/MA165/HW2.txt ~/Homework/MA165/HW1.txt
+mv Homework/MA165/HW2.txt Homework/MA165/HW1.txt
 
 # Task 2
 rm Memes/SpongeBob/money.jpeg
 
 # Task 3
-mv Memes/'Start Wars'/fafs/jpg Memes/SpongeBob
+mv Memes/'Star Wars'/fafsa.jpg Memes/SpongeBob
 
 # Task 4
 rm -r Homework/CS191
@@ -19,10 +19,10 @@ rm -r Homework/CS191
 mkdir Programs/C++
 
 # Task 6
-mv Programs/Age.cpp ~/Programs/C++/Age.cpp
+mv Programs/Age.cpp Programs/C++/Age.cpp
 
 # Task 7
-cp -r Programs/Age.cpp C++
+cp -r Programs/Java Homework/CS180/Project_1
 
 # Task 8
 mv Programs/Count.c Programs/Goodbye.c Programs/C
@@ -34,9 +34,7 @@ mv Programs/Name.py Programs/Space.py Programs/'Python 3'
 touch Homework/POL141/Essay.txt
 
 # Task 11
- ls
-ORIGIN	   SSH_Shortcut.md  Terminal_Guide.md  clean.sh   turn-in.sh
-README.md  Screenshots	    answers.sh	       grader.sh
+ls ..
 
 # Task 12
 : '
@@ -44,6 +42,5 @@ Type your vim/nano customizations below:
 let mapleader=',' 
 set autoread  
 set nobackup noswapfile nowritebackup    
-
-
 '
+
